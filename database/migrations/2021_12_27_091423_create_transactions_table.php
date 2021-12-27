7a17e5b6bdea58b33bc47f47ceaaed63f390e23c
aronei44 <meliodasmeliodas224@gmail.com>
2021-12-27 16:20:13 +0700

    add transaction model and migration

---
+++ database/migrations/2021_12_27_091423_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('product_name');
+            $table->string('product_price');
+            $table->string('product_amount');
+            $table->string('total');
+            $table->enum('status',['created','process'])->default('created');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
